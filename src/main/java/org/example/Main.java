846dd202d6541f3136abbed4ecf0a27cf8b9843e
Olivia-Pigani <piganiolivia@gmail.com>
2023-12-27 18:22:07 +0100

    java app is now dockerized, can do a successful connexion !!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,42 @@
 package org.example;
 
+import org.example.utils.DatabaseSingleton;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = DatabaseSingleton.getConnection();
+            statement = connection.createStatement();
+            String query = "SELECT * FROM characters"; // Remplacez 'ma_table' par le nom de votre table
+            resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                // Exemple : Si votre table a des colonnes 'id' et 'nom'
+                int id = resultSet.getInt("id");
+                String nom = resultSet.getString("name");
+
+                System.out.println("ID: " + id + ", Nom: " + nom);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null) resultSet.close();
+                if (statement != null) statement.close();
+                // Ne fermez pas la connexion si vous l'utilisez ailleurs dans l'application
+                // DatabaseSingleton.closeConnection();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/main/java/org/example/utils/DatabaseSingleton.java
+++ src/main/java/org/example/utils/DatabaseSingleton.java
@@ -7,22 +7,30 @@ import java.sql.SQLException;
 public class DatabaseSingleton {
 
 
+
     private static final String dbName = System.getenv("POSTGRES_DB");
-    private static final String URI = "jdbc:postgresql://localhost:5432/" + dbName;
-    private static final String USER = System.getenv("POSTGRES_USER")  ;
+    private static final String USER = System.getenv("POSTGRES_USER");
     private static final String PASSWORD = System.getenv("POSTGRES_PASSWORD");
+    // private static final String PASSWORD = "root";
+    private static final String URI = "jdbc:postgresql://db:5432/" + dbName;
+    //private static final String USER = "postgres";
     private static volatile Connection connection = null;
 
     private DatabaseSingleton() {
     }
 
-    public static Connection getConnection(){
-        if (connection == null){
-            synchronized (DatabaseSingleton.class){
-                if (connection == null ){
+    public static Connection getConnection() {
+
+        System.out.println(URI);
+        System.out.println(USER);
+        System.out.println(PASSWORD);
+
+        if (connection == null) {
+            synchronized (DatabaseSingleton.class) {
+                if (connection == null) {
                     try {
                         connection = DriverManager.getConnection(URI,USER,PASSWORD);
-                    }catch (SQLException e){
+                    } catch (SQLException e) {
                         e.printStackTrace();
                     }
                 }
@@ -31,15 +39,15 @@ public class DatabaseSingleton {
         return connection;
     }
 
-    public static void closeConnection(){
-        if (connection != null){
-            synchronized (DatabaseSingleton.class){
-                if (connection != null){
+    public static void closeConnection() {
+        if (connection != null) {
+            synchronized (DatabaseSingleton.class) {
+                if (connection != null) {
                     try {
                         connection.close();
-                    }catch (SQLException e){
+                    } catch (SQLException e) {
                         e.printStackTrace();
-                    }finally {
+                    } finally {
                         connection = null;
                     }
                 }
