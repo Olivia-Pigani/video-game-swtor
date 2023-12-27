2dbec6f78a6401d7d09d37c8a978c0ff56f4f150
Olivia-Pigani <piganiolivia@gmail.com>
2023-12-27 18:22:45 +0100

    java app is now dockerized, can do a successful connexion !!

--- src/main/java/org/example/utils/DatabaseSingleton.java
+++ src/main/java/org/example/utils/DatabaseSingleton.java
@@ -11,9 +11,7 @@ public class DatabaseSingleton {
     private static final String dbName = System.getenv("POSTGRES_DB");
     private static final String USER = System.getenv("POSTGRES_USER");
     private static final String PASSWORD = System.getenv("POSTGRES_PASSWORD");
-    // private static final String PASSWORD = "root";
     private static final String URI = "jdbc:postgresql://db:5432/" + dbName;
-    //private static final String USER = "postgres";
     private static volatile Connection connection = null;
 
     private DatabaseSingleton() {
